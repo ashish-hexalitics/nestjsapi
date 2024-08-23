fff7ddaf23151e19c66e868c1c37e0b64d6bb09d
ashish <ashish.vishwakarma@hexalitics.com>
2024-08-23 20:08:58 +0530

    created resume page for user'

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { urlencoded, json } from 'express';
 
 
 async function bootstrap() {
@@ -14,7 +15,8 @@ async function bootstrap() {
 
   // Enable validation globally
   app.useGlobalPipes(new ValidationPipe());
-
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(3000);
 }
 bootstrap();

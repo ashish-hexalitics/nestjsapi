475bc9fb0c03799cbb8228f48cc32c6962c542de
ashish <ashish.vishwakarma@hexalitics.com>
2024-09-03 18:52:55 +0530

    api permission

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { urlencoded, json } from 'express';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://ec2-13-60-22-180.eu-north-1.compute.amazonaws.com:3000',
+    // origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });

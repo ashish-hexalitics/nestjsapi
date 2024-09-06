6b49b74f662a1bc7406f186203f525197dc36642
ashish <ashish.vishwakarma@hexalitics.com>
2024-09-06 13:51:00 +0530

    added signup signin page

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://whimsical-paletas-3b0383.netlify.app/*',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
